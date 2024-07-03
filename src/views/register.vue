946eb862fefe3b11ba710a1ac26c12d7952e2892
nanako <961870173@qq.com>
2024-07-03 20:46:33 +0800

    表单验证修复

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,1 +1,170 @@
-<template></template>
+<template>
+	<div class="page-container">
+		<el-form
+			:model="reigsterForm"
+			class="reigster-container"
+			:rules="rules"
+			ref="ruleRef">
+			<h3 class="title">说的道理管理系统</h3>
+			<el-form-item label="身份证号" prop="idCard">
+				<el-input
+					placeholder="请输入身份证号"
+					v-model="reigsterForm.idCard"
+					class="input-item"></el-input>
+			</el-form-item>
+			<el-form-item label="密码" prop="password">
+				<el-input
+					placeholder="请输入密码"
+					v-model="reigsterForm.password"
+					class="input-item"
+					show-password></el-input>
+			</el-form-item>
+			<el-form-item label="身份" prop="isDocter">
+				<el-select
+					v-model="reigsterForm.isDocter"
+					placeholder="选择身份"
+					class="input-item">
+					<el-option
+						v-for="item in options"
+						:key="item.value"
+						:label="item.label"
+						:value="item.value" />
+				</el-select>
+			</el-form-item>
+
+			<!-- 根据身份动态显示不同的表单项 -->
+			<template v-if="reigsterForm.isDocter === true">
+				<el-form-item label="医生凭证编号" prop="certificate">
+					<el-input
+						placeholder="请输入医生凭证编号"
+						v-model="reigsterForm.certificate"
+						class="input-item"></el-input>
+				</el-form-item>
+				<el-form-item label="专长" prop="specialty">
+					<el-input
+						placeholder="请输入专长"
+						v-model="reigsterForm.specialty"
+						class="input-item"></el-input>
+				</el-form-item>
+				<el-form-item label="地理位置" prop="location">
+					<el-input
+						placeholder="请输入地理位置"
+						v-model="reigsterForm.location"
+						class="input-item"></el-input>
+				</el-form-item>
+			</template>
+
+			<template v-else-if="reigsterForm.isDocter === false">
+				<el-form-item label="联系方式" prop="contact">
+					<el-input
+						placeholder="请输入联系方式"
+						v-model="reigsterForm.contact"
+						class="input-item"></el-input>
+				</el-form-item>
+			</template>
+
+			<div class="button-container">
+				<el-button class="button">注册</el-button>
+			</div>
+		</el-form>
+	</div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+import useUserStore from "@/store/module/user"
+import usePermissionStore from "@/store/module/permission"
+
+const userStore = useUserStore()
+const permissionStore = usePermissionStore()
+const router = useRouter()
+const reigsterForm = ref({
+	password: "",
+	isDocter: null,
+	idCard: "",
+	certificate: "",
+	specialty: "",
+	location: "",
+	contact: "",
+	medicalRecords: "",
+})
+
+const options = [
+	{ value: true, label: "医生" },
+	{ value: false, label: "患者" },
+]
+
+const rules = {
+	password: [
+		{ required: true, message: "请输入密码", trigger: "blur" },
+		{ max: 30, message: "密码最多30位", trigger: "blur" },
+	],
+	isDocter: [{ required: true, message: "请选择身份", trigger: "blur" }],
+	idCard: [{ required: true, message: "请输入身份证ID", trigger: "blur" }],
+	certificate: [
+		{ required: true, message: "请输入医生凭证编号", trigger: "blur" },
+	],
+	specialty: [{ required: true, message: "请输入专长", trigger: "blur" }],
+	location: [{ required: true, message: "请输入地理位置", trigger: "blur" }],
+	contact: [{ required: true, message: "请输入联系方式", trigger: "blur" }],
+}
+
+</script>
+
+<style>
+body,
+html {
+	margin: 0;
+	padding: 0;
+	height: 100%;
+	overflow: auto;
+}
+.page-container {
+	background-color: #e0f2f1;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 0;
+	margin: 0;
+}
+
+.reigster-container {
+	background-color: #fff;
+	padding: 40px;
+	border-radius: 10px;
+	width: 400px;
+	box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+	text-align: center;
+}
+
+.title {
+	margin-bottom: 30px;
+	font-size: 2em;
+	color: #333;
+}
+
+.input-item {
+	flex: 1;
+	font-size: 1.1em;
+}
+
+.button-container {
+	display: flex;
+	justify-content: center;
+}
+
+.button {
+	margin-top: 10px;
+	width: 48%;
+	font-size: 1.1em;
+	background-color: #42b983;
+	color: #fff;
+	margin: 0 5px;
+}
+
+.button:hover {
+	background-color: #339970;
+}
+</style>
